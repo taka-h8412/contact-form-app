a1b39941b7bfb6ad101edf43e723d46a8ac5a87d
taka.h <hitoshi.t.biz@gmail.com>
2026-06-24 16:17:31 +0900

    fix: Fortify認証後遷移と日本語バリデーションを調整

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -14,6 +14,6 @@ trait PasswordValidationRules
      */
     protected function passwordRules(): array
     {
-        return ['required', 'string', Password::default(), 'confirmed'];
+        return ['required', 'string', 'min:8', 'confirmed'];
     }
 }
